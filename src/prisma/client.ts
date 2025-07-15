8ca9ed98ca6126b27ed31ea37f9c3338fc147bc2
Mehdi Belkhelfa <midou8687@gmail.com>
2025-07-15 14:40:07 +0200

    refactor: revert to standard PrismaClient import and remove extension

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,4 +1,2 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-
-export const prisma = new PrismaClient().$extends(withAccelerate())
+import { PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient();
